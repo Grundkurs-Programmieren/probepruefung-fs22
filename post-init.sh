5f88b15d023e45a27c1ae59cd4eb935fe8e2d51c
Noe Thalheim <ten1@bfh.ch>
2022-05-03 11:46:03 +0200

    chore: add template files

---
+++ post-init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /home/jovyan/work/.jupyter_config/lab/user-settings/@jupyterlab/toc-extension
+cp ./.user-settings/@jupyterlab/toc-extension/plugin.jupyterlab-settings /home/jovyan/work/.jupyter_config/lab/user-settings/@jupyterlab/toc-extension/plugin.jupyterlab-settings
